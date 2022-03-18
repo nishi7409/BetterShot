643394941a96a1ee62115b5fb50054a5333566e9
Vincent Chen <vincentqchen@outlook.com>
2022-03-18 01:39:36 -0400

    Test Printing out inputs

--- frontend/pages/auth/register.vue
+++ frontend/pages/auth/register.vue
@@ -9,30 +9,30 @@
                         <div class="field">
                             <label class="label">First Name</label>
                             <div class="control">
-                            <input class="input" type="text" placeholder="John">
+                            <input v-model="firstName" class="input" type="text" placeholder="John">
                             </div>
                         </div>
                         <div class="field">
                             <label class="label">Last Name</label>
                             <div class="control">
-                            <input class="input" type="text" placeholder="Doe">
+                            <input v-model="lastName" class="input" type="text" placeholder="Doe">
                             </div>
                         </div>
                         <div class="field">
                             <label class="label">Email</label>
                             <div class="control">
-                            <input class="input" type="email" placeholder="e.g. vincent@example.com">
+                            <input v-model="email" class="input" type="email" placeholder="e.g. vincent@example.com">
                             </div>
                         </div>
 
                         <div class="field">
                             <label class="label">Password</label>
                             <div class="control">
-                            <input class="input" type="password" placeholder="********">
+                            <input v-model="password"  class="input" type="password" placeholder="********">
                             </div>
                         </div>
 
-                        <button class="button is-primary">Sign Up</button>
+                        <button class="button is-primary" @click="printToConsole()">Sign Up</button>
                     </form>
                     </div>
                 </div>
@@ -48,17 +48,14 @@ export default {
       }
    },
    methods: {
-      // dispatches login handler with username & password parameters
-      login() {
-         store.dispatch(type.login, {"username": this.username, "password": this.password})
-      }
+    //   // dispatches login handler with username & password parameters
+    //   login() {
+    //      store.dispatch(type.login, {"username": this.username, "password": this.password})
+    //   }
+        printToConsole() {
+            console.log("Info: "+this.firstName+"-"+this.lastName+"-"+this.email+"-"+this.password);
+        }
    },
-   computed: {
-      // token key for local storage
-      ...mapState({
-         sessionKey: 'token',
-      })
-   }
 };
 </script>
 
