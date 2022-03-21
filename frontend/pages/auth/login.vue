a770606dc91add2db8b6e32576808b3961d42dfd
nishi7409 <nishi7409@gmail.com>
2022-03-21 14:44:12 -0400

    fix

--- frontend/pages/auth/login.vue
+++ frontend/pages/auth/login.vue
@@ -1,43 +1,58 @@
 <template>
 <div id="login">
-
-            <div id="login-form" class="columns is-mobile is-centered" margin-top>
-                <div class="column is-half">
-                    <div class="container">
-                    <form class="box has-background-grey-light">
-                        <h1 class="title"> Login </h1>
-                        <div class="field">
-                            <label class="label">Email</label>
-                            <div class="control">
-                            <input class="input" type="email" placeholder="e.g. vincent@example.com">
-                            </div>
-                        </div>
-
-                        <div class="field">
-                            <label class="label">Password</label>
-                            <div class="control">
-                            <input class="input" type="password" placeholder="********">
-                            </div>
-                        </div>
-
-                        <button class="button is-primary">Sign in</button>
-                    </form>
+    <div id="login-form" class="columns is-mobile is-centered" margin-top>
+        <div class="column is-half">
+            <div class="container">
+            <form class="box has-background-grey-light">
+                <h1 class="title"> Login </h1>
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control">
+                    <input class="input" type="email" placeholder="john@doe.com" v-model="email">
                     </div>
                 </div>
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                    <input class="input" type="password" placeholder="********" v-model="password">
+                    </div>
+                </div>
+                <button class="button is-primary" v-on:click="login()">Sign in</button>
+            </form>
             </div>
+        </div>
+    </div>
 </div>
 </template>
 
 <style>
-#login{
-    background-color: rgb(10,196,131);
-    height: 100vh;
-}
+    #login{
+        background-color: rgb(10,196,131);
+        height: 100vh;
+    }
 
-#login-form{
-    position: relative;
-    top: 20vh;
-}
-
-
+    #login-form{
+        position: relative;
+        top: 20vh;
+    }
 </style>
+
+
+<script>
+
+    export default {
+        name: 'Login',
+        data: function() {
+            return {
+
+            }
+        },
+        methods: {
+            login() {
+                console.log(this.email)
+                console.log(this.password)
+            }
+        }
+    }
+
+</script>
