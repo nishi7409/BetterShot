1864f8204670275d25e300d4741599a9431af7a2
nishi7409 <nishi7409@gmail.com>
2022-03-26 12:34:16 -0400

    url shortener

---
+++ server/urlShortener/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+# Import the home view
+from .views import home_view
+
+appname = "shortener"
+
+urlpatterns = [
+    # Home view
+    path("", home_view, name="home")
+]
