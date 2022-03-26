ee9cb0dec06931e299b9e380a54f8340426ea0af
nishi7409 <nishi7409@gmail.com>
2022-03-26 12:29:03 -0400

    base url shortener stuff

--- server/urlShortener/views.py
+++ server/urlShortener/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render, get_object_or_404 # We will use it later
+
+from django.http import HttpResponse 
+
+# Create your views here.
+
+def home_view(request):
+    return HttpResponse("Hello world")
