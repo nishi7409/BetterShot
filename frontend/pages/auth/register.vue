0e1b8f2a105b5e64ddaacb34c154354054042ea8
Maxwell Tang <91094317+mackstey28@users.noreply.github.com>
2022-03-28 22:18:38 -0400

    Update register.vue

--- frontend/pages/auth/register.vue
+++ frontend/pages/auth/register.vue
@@ -1,74 +1,83 @@
 <template>
-  <div id="login">
-    <div id="login-form" class="columns is-mobile is-centered" margin-top>
-      <div class="column is-half">
-        <div class="container">
-          <form class="box has-background-grey-light">
-            <h1 class="title">Login</h1>
-            <div class="field">
-              <label class="label">Email</label>
-              <div class="control">
-                <input
-                  class="input"
-                  type="email"
-                  placeholder="john@doe.com"
-                  v-model="userData.email"
-                  required
-                />
-              </div>
+<div id="register">
+    <div id="register-form" class="columns is-mobile is-centered" margin-top>
+        <div class="column is-half">
+            <div class="container">
+            <form class="box has-background-grey-light">
+                <h1 class="title"> Register </h1>
+                <div class="field">
+                    <label class="label">First Name</label>
+                    <div class="control">
+                    <input v-model="firstName" class="input" type="text" placeholder="John">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Last Name</label>
+                    <div class="control">
+                    <input v-model="lastName" class="input" type="text" placeholder="Doe">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Email</label>
+                    <div class="control">
+                    <input v-model="userData.email" class="input" type="email" placeholder="e.g. john@doe.com">
+                    </div>
+                </div>
+
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                    <input v-model="userData.password"  class="input" type="password" placeholder="********">
+                    </div>
+                </div>
+                <div class="field">
+                    <label class="label">Password</label>
+                    <div class="control">
+                    <input v-model="userData.password2"  class="input" type="password" placeholder="********">
+                    </div>
+                </div>
+                <button class="button is-primary" v-on:click="signUp(userData)">Sign Up</button>
+            </form>
             </div>
-            <div class="field">
-              <label class="label">Password</label>
-              <div class="control">
-                <input
-                  class="input"
-                  type="password"
-                  placeholder="********"
-                  v-model="userData.password"
-                  required
-                />
-              </div>
-            </div>
-            <button class="button is-primary" @click="login(userData)">Sign in</button>
-          </form>
         </div>
-      </div>
     </div>
-  </div>
+</div>
 </template>
 
+<script>
+    export default {
+        name: 'Register',
+        data: () => ({
+            userData: {
+                email: '', password: '', password2: ''
+            }
+        }),
+        methods: {
+            async signUp(registrationInformation) {
+                await this.$axios
+                    .post('accounts/users/', registrationInformation)
+                    .then((response) => {
+                        console.log('Successful')
+                    })
+                    .catch((error) => {
+                        console.log('errors:', error.response)
+                    })
+                this.$auth.loginWith('local', {
+                    data: registrationInformation
+                })
+            },
+        },
+    };
+</script>
+
 <style>
-#login {
-  background-color: rgb(10, 196, 131);
-  height: 100vh;
+#register{
+    background-color: rgb(10,196,131);
+    height: 100vh;
 }
 
-#login-form {
-  position: relative;
-  top: 20vh;
+#register-form{
+    position: relative;
+    top: 100px;
 }
 </style>
-
-
-<script>
-export default {
-  name: 'Login',
-  data: () => ({
-    userData: {email: '', password: ''},
-  }),
-  methods: {
-    // following this https://dev.to/ignisda/setting-up-user-authentication-with-nuxtjs-and-django-rest-framework-part-2-2dkm
-      async login(userData) {
-        alert(userData.email)
-        try {
-          await this.$auth.loginWith('local', {
-            data: userData,
-          })
-          alert('successful')
-        } catch (error) {
-          alert('unsuccessful')
-        }
-      },
-    },
-  }
-</script>
