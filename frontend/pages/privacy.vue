53dd5a55f6368f717dbd4300126fa67edbb8d66b
Maxwell Tang <91094317+mackstey28@users.noreply.github.com>
2022-04-05 14:43:08 -0400

    Create privacy.vue

---
+++ frontend/pages/privacy.vue
@@ -0,0 +1,65 @@
+<!-- cd /mnt/c/users/maxwe/OneDrive/Documents/BetterShot-master/frontend -->
+<!-- npm run dev -->
+
+<template>
+  <div class="Profile Settings">
+
+    <style>
+      body {
+        background-image: url('https://coolbackgrounds.io/images/backgrounds/index/compute-ea4c57a4.png');
+        background-attachment: fixed;
+      }
+      .field-name {
+        margin-top: 20px;
+        font-size: 20px;
+        color: #B5B5BC;
+      }
+      .field-value {
+        margin-bottom: 7px;
+        font-size: 20px;
+        color: #4A4A4A;
+      }
+      .inner {
+        display: inline-block;
+      }
+      .tab { 
+        margin-bottom: 10px; 
+      }
+      .box-background {
+        background-color: #1f2330 ;
+      }
+    </style>
+
+    <div class="box">
+      <h1 class="title">Privacy Agreement</h1>
+    </div>
+
+    <div class="box">
+      <figure class="image is-128x128">
+        <img src="https://cdn.discordapp.com/attachments/930636851667140708/930637064666505256/unknown.png">
+      </figure>
+    </div>
+
+    <div class="box">
+      <p> 
+        BetterShot will not share your information with any other parties. We do not collect any information that you do not give us, such as tracking and cookies.
+      </p>
+      <p>
+        Your personal information is strictly confidential and unviewable by any third party, unless you would like to share it.
+      </p>
+      <p>
+        Any files you upload to our servers will not be shared in anyway with any third party, unless you would like to voluntarily share it.
+      </p>
+      <p>
+        BetterShot is not held legally responsibly for any illegal or inappropriate files uploaded to our servers by any member not in our development team.
+      </p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  
+}
+</script>
